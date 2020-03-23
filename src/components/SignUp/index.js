fa618799f2678681c96307b315ec3404aec883fd
Code-Apprentice <codeguru42@gmail.com>
2020-03-22 23:34:05 -0600

    Add signup form

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -3,6 +3,14 @@ import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
 
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+};
+
 const SignUpPage = () => (
     <div>
         <h1>SignUp</h1>
@@ -13,17 +21,57 @@ const SignUpPage = () => (
 class SignUpForm extends Component {
     constructor(props) {
         super(props);
+        this.state = { ...INITIAL_STATE };
     }
 
     onSubmit = event => {
     };
 
     onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
     };
 
     render() {
+        const {
+            username,
+            email,
+            passwordOne,
+            passwordTwo,
+            error,
+        } = this.state;
+
         return (
             <form onSubmit={this.onSubmit}>
+                <input
+                    name="username"
+                    value={username}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Full Name"
+                />
+                <input
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    name="passwordOne"
+                    value={passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                <input
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button type="submit">Sign Up</button>
+                {error && <p>{error.message}</p>}
             </form>
         );
     }
