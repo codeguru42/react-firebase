b009681dcf32efb690d2d392599c723992232fb7
Code-Apprentice <codeguru42@gmail.com>
2020-03-22 23:09:12 -0600

    Stub out a signup form

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
-const SignUp = () => (
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
+
+const SignUpPage = () => (
     <div>
         <h1>SignUp</h1>
+        <SignUpForm />
     </div>
 );
-export default SignUp;
+
+class SignUpForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    onSubmit = event => {
+    };
+
+    onChange = event => {
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+            </form>
+        );
+    }
+}
+
+const SignUpLink = () => (
+    <p>
+        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+);
+
+export default SignUpPage;
+export { SignUpForm, SignUpLink };
