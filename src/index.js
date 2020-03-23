03a4f3342eb4dc3721b1a5f0c0e706fbf0f415ad
Code-Apprentice <codeguru42@gmail.com>
2020-03-22 21:27:13 -0600

    Add FirebaseContext

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+import App from './components/App';
+import Firebase, {FirebaseContext} from "./components/Firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <FirebaseContext.Provider value={new Firebase()}>
+          <App />
+      </FirebaseContext.Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
