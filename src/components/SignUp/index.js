d59f4f1d77630865f6ef183e4102e6ef437db19e
Code-Apprentice <codeguru42@gmail.com>
2020-03-23 00:41:32 -0600

    Compose higher order components

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
@@ -103,7 +104,10 @@ const SignUpLink = () => (
     </p>
 );
 
-const SignUpForm = withRouter(withFirebase(SignUpFormBase));
+const SignUpForm = compose(
+    withRouter,
+    withFirebase,
+)(SignUpFormBase);
 
 export default SignUpPage;
 export { SignUpForm, SignUpLink };
