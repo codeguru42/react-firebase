8bc8051edd9a5233beb5327565edcf9234070084
Code-Apprentice <codeguru42@gmail.com>
2020-03-28 20:34:33 -0600

    Show user's email on account page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import { AuthUserContext, withAuthorization } from '../Session';
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
-import { withAuthorization } from '../Session';
 
 const AccountPage = () => (
-    <div>
-        <h1>Account Page</h1>
-        <PasswordForgetForm />
-        <PasswordChangeForm />
-    </div>
+    <AuthUserContext.Consumer>
+        {authUser => (
+            <div>
+                <h1>Account: {authUser.email}</h1>
+                <PasswordForgetForm />
+                <PasswordChangeForm />
+            </div>
+        )}
+    </AuthUserContext.Consumer>
 );
 
 const condition = authUser => !!authUser;
