c60c92a946cf925d6d77905fb0f1fa12308605cb
Code-Apprentice <codeguru42@gmail.com>
2020-03-30 07:59:55 -0600

    Fix .then() chain when creating a new user

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -31,10 +31,6 @@ class SignUpFormBase extends Component {
         this.props.firebase
             .doCreateUserWithEmailAndPassword(email, passwordOne)
             .then(authUser => {
-                this.setState({ ...INITIAL_STATE });
-                this.props.history.push(ROUTES.HOME);
-            })
-            .then(authUser => {
                 // Create a user in your Firebase realtime database
                 return this.props.firebase
                     .user(authUser.user.uid)
@@ -42,6 +38,10 @@ class SignUpFormBase extends Component {
                         username,
                         email,
                     });
+            })
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.HOME);
             })
             .catch(error => {
                 this.setState({ error });
